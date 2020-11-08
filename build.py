3b73b469202691ee23e67e270509279f75e71dc3
NessMC <contact@nessmc.fr>
2020-11-08 13:51:31 +0100

    🔥 Unused compilation part removed in build script.

--- build.py
+++ build.py
@@ -7,28 +7,11 @@ def build():
   if len(npmPath) == 0:
     print('No NPM installations found. Aborting...')
     return
-  # Listing and filtering commands in NPM path
-  files = listdir(npmPath[0])
-  commands = list(filter(lambda x: not x.__contains__('.'), files))
-  if len(commands) == 0:
-    print('No NPM installations found. Aborting...')
-    return
-  # Choosing installation command
-  print('Checking for Yarn installation...')
-  shellCommand = 'yarn' if commands.__contains__('yarn') else 'npm'
-  # Installing modules
-  print('Installing needed modules...')
-  system(shellCommand + ' install')
-  # Compiling project
-  print('Compiling project into javascript...')
-  system(shellCommand + ' tsc')
   # Installing globally
   try:
-    open('./dist/main.js')
     print('Installing globally CLI...')
     system('npm i -g')
   except:
-    print('No compiled output found. Aborting...')
     return
 
 build()
