a5815b3aa56284a3018f658f849491195c54c91b
NessMC <contact@nessmc.fr>
2020-11-08 12:24:55 +0100

    🚸 Build script messages printing added.

--- build.py
+++ build.py
@@ -2,25 +2,33 @@ from os import environ, listdir, system
 
 def build():
   # Checking and getting NPM path.
+  print('Checking for NPM Paths...')
   npmPath = list(filter(lambda x: x.__contains__('npm'), environ['PATH'].split(';')))
   if len(npmPath) == 0:
+    print('No NPM installations found. Aborting...')
     return
   # Listing and filtering commands in NPM path
   files = listdir(npmPath[0])
   commands = list(filter(lambda x: not x.__contains__('.'), files))
   if len(commands) == 0:
+    print('No NPM installations found. Aborting...')
     return
   # Choosing installation command
+  print('Checking for Yarn installation...')
   shellCommand = 'yarn' if commands.__contains__('yarn') else 'npm'
   # Installing modules
+  print('Installing needed modules...')
   system(shellCommand + ' install')
   # Compiling project
+  print('Compiling project into javascript...')
   system(shellCommand + ' tsc')
   # Installing globally
   try:
     open('./dist/main.js')
+    print('Installing globally CLI...')
     system('npm i -g')
   except:
+    print('No compiled output found. Aborting...')
     return
 
 build()
