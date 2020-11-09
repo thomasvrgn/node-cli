d12289fa76c5a51adabf7edfdbff16f87401583b
thomasvergne <thomas@quark-lang.dev>
2020-11-09 18:57:38 +0100

    🔥 Build script NPM path checking removed.

--- build.py
+++ build.py
@@ -1,12 +1,6 @@
 from os import environ, listdir, system
 
 def build():
-  # Checking and getting NPM path.
-  print('Checking for NPM Paths...')
-  npmPath = list(filter(lambda x: x.__contains__('npm'), environ['PATH'].split(';')))
-  if len(npmPath) == 0:
-    print('No NPM installations found. Aborting...')
-    return
   # Installing globally
   try:
     print('Installing globally CLI...')
